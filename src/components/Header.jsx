ad82bd5bc1dff5562e72d2d5ace92bc8d922da8e
nour <nourwael1530@gmail.com>
2022-11-05 01:16:50 +0200

    fixed some issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = ({ category, title }) => {
   return (
-    <div className="mb-10">
-      <p className="text-gray-400">{category}</p>
+    <div className="mb-10 mt-12">
+      <p className="text-gray-400 text-lg">{category}</p>
       <p className="text-3xl font-extrabold tracking-tight text-slate-900">
         {title}
       </p>
